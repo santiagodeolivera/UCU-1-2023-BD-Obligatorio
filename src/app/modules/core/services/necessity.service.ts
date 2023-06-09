c8b464bc2d659a658cbe5ac049ceb4000cf0244d
Walter <Tano.walter@hotmail.com>
2023-06-08 22:58:44 -0300

    Added necessity service

--- src/app/modules/core/services/necessity.service.ts
+++ src/app/modules/core/services/necessity.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs'
+
+import { IHTTPResponse, INecessity } from 'src/app/modules/core/interfaces';
 
 @Injectable({
   providedIn: 'root'

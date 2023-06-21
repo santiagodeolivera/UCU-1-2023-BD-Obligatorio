f2bb2138214b7de8eed0fca8a9412deb787be75f
Walter <Tano.walter@hotmail.com>
2023-06-21 01:05:13 -0300

    añadí search form base

--- src/app/modules/pages/search/components/necessity-search-form/necessity-search-form.component.ts
+++ src/app/modules/pages/search/components/necessity-search-form/necessity-search-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+
+import { INecessitySearchRequest, ISkill } from 'src/app/modules/core/interfaces';
 
 @Component({
   selector: 'app-necessity-search-form',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NecessitySearchFormComponent implements OnInit {
 
-  constructor() { }
+  skillOptions: ISkill[] = [];
+  necessitySearchForm = this.fb.group({
+    searchTerm: new FormControl<string | undefined>(undefined),
+    skills: new FormControl<string[] | undefined>(undefined),
+    minStartDate: new FormControl<Date | undefined>(undefined),
+    maxStartDate: new FormControl<Date | undefined>(undefined),
+    minEndDate: new FormControl<Date | undefined>(undefined),
+    maxEndDate: new FormControl<Date | undefined>(undefined)
+  });
+
+  @Output() search = new EventEmitter<INecessitySearchRequest>();
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.getSkillOptions();
   }
 
+  getSkillOptions() {
+    // TODO: Get all skills for options.
+  }
+
+  handleSubmit() {
+    const { skills, searchTerm, minStartDate, maxStartDate, minEndDate, maxEndDate } = this.necessitySearchForm.value;
+
+    const searchRequest: INecessitySearchRequest = {
+      skills: skills || undefined,
+      searchTerm: searchTerm || undefined,
+      startDate: (minStartDate || maxStartDate) && {
+        min: minStartDate || undefined,
+        max: maxStartDate || undefined
+      } || undefined,
+      endDate: (minEndDate || maxEndDate) && {
+        min: minEndDate || undefined,
+        max: maxEndDate || undefined
+      } || undefined
+    };
+
+    this.search.emit(searchRequest);
+  }
 }
--- src/app/modules/pages/search/pages/necessity-search/necessity-search.component.ts
+++ src/app/modules/pages/search/pages/necessity-search/necessity-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ISearchResult } from 'src/app/modules/core/interfaces';
+import { INecessitySearchRequest, ISearchResult } from 'src/app/modules/core/interfaces';
+import { NecessityService } from 'src/app/modules/core/services/necessity.service';
 
 @Component({
   selector: 'app-necessity-search',
@@ -8,11 +9,28 @@ import { ISearchResult } from 'src/app/modules/core/interfaces';
 })
 export class NecessitySearchComponent implements OnInit {
 
+  isLoading: boolean = false;
   searchResults?: ISearchResult[];
 
-  constructor() { }
+  constructor(
+    private necessityService: NecessityService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  handleSearch($event: INecessitySearchRequest) {
+    this.isLoading = true;
+    this.necessityService.getNecessitiesByFilters($event)
+    .subscribe(response => {
+      this.isLoading = false;
+
+      if (response.success) {
+        this.searchResults = response.data!;
+        return;
+      }
+
+      // TODO: SHOW ERROR
+    });
+  }
 }

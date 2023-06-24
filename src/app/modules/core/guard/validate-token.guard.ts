8b0c90cedd1e093e1ba54bc7d74bd5675addc5c6
Walter <Tano.walter@hotmail.com>
2023-06-24 00:50:02 -0300

    agregue guards a routing

--- src/app/modules/core/guard/validate-token.guard.ts
+++ src/app/modules/core/guard/validate-token.guard.ts
@@ -20,7 +20,7 @@ export class ValidateTokenGuard implements CanActivate, CanLoad {
     return this.authService.validateToken().pipe(
       tap((valid) => {
         if (!valid) {
-          this.router.navigateByUrl('/');
+          this.router.navigateByUrl('/login');
         }
       })
     );

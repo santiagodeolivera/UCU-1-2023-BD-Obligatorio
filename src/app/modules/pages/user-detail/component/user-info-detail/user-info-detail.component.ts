f5f1ad7778320ebb740a6be384d709266da72c44
jumpert <juan.perezc@correo.ucu.edu.uy>
2023-06-25 15:20:35 -0300

    faltan cosas

---
+++ src/app/modules/pages/user-detail/component/user-info-detail/user-info-detail.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { GoogleMapsGeolocation } from 'src/app/modules/core/classes';
+import { IUser } from 'src/app/modules/core/interfaces';
+import { AuthService } from 'src/app/modules/core/services/auth.service';
+import { MapService } from 'src/app/modules/core/services/map.service';
+
+@Component({
+  selector: 'app-user-info-detail',
+  templateUrl: './user-info-detail.component.html',
+  styleUrls: ['./user-info-detail.component.scss']
+})
+export class UserInfoDetailComponent implements OnInit {
+  @Input() user!: IUser;
+
+  constructor(
+    private mapService: MapService,
+    private authService: AuthService,
+
+  ) { }
+
+  ngOnInit(): void {
+  }
+  get locationString(): string { 
+    const location = this.user.address;
+    let locationString = location?.streetAddress ? location.streetAddress : '';
+    locationString = location?.city ? `${locationString}, ${location?.city}` : locationString;
+    locationString = location?.province ? `${locationString}, ${location?.province}` : locationString;
+    locationString = location?.country ? `${locationString}, ${location?.country}` : locationString;
+
+    return locationString ? locationString : `${location?.latitude}, ${location?.longitude}`;
+  }
+
+  get isByRunningUser(): boolean {
+    return this.user.id !== this.authService.runningUser?.id;
+  }
+
+  getUserLocation() {
+    if (!(this.user.address?.latitude && this.user.address.longitude)) return;
+
+    this.mapService.getPlaceInformationFromCoordinates(
+      this.user.address?.latitude,
+      this.user.address?.longitude,
+      GoogleMapsGeolocation
+    )
+    .subscribe(result => {
+      this.user.address = result;
+    });
+  }
+
+
+
+}
